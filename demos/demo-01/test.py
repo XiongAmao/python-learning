62bfe6d82c6bbecbb45cc6d1607631d2022c5bba
XiongAmao <onycat@live.cn>
2018-08-01 17:50:08 +0800

    update

--- demos/demo-01/test.py
+++ demos/demo-01/test.py
@@ -1,2 +1,13 @@
-#!/usr/bin/python
-print('yes')
+
+a = 321
+b = 123
+print('a = ', a)
+print('b = ', b)
+print('\n')
+print('a + b :', a + b)
+print('a - b:', a - b)
+print('a * b:', a * b)
+print('a / b:', a / b)
+print('a // b:', a // b)
+print('a % b:', a % b)
+print('a ** b:', a ** b)
---
+++ lessons/README.md
@@ -0,0 +1,3 @@
+# Record python learning
+
+lessons from [Python - 100天从新手到大师](https://github.com/jackfrued/Python-100-Days)
